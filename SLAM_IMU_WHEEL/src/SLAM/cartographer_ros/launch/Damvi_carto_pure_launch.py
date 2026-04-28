10d8ae085867c5d8418fb6072729a902bb8f9574
sygom_02 <seongyoungmoon@gmail.com>
2026-04-28 17:14:52 +0900

    sim_time_false

--- SLAM_IMU_WHEEL/src/SLAM/cartographer_ros/launch/Damvi_carto_pure_launch.py
+++ SLAM_IMU_WHEEL/src/SLAM/cartographer_ros/launch/Damvi_carto_pure_launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
         
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='true',
+            default_value='false',
             description='Use simulation time if true'
         ),
         
