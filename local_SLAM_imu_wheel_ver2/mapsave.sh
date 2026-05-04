c4f9ccbe555968c9f577da3f7893693ef282e6e2
sygom_02 <seongyoungmoon@gmail.com>
2026-05-04 17:30:38 +0900

    able to change base_complete

--- local_SLAM_imu_wheel_ver2/mapsave.sh
+++
@@ -1,5 +0,0 @@
-source /home/rcv/SLAM/install/setup.bash
-
-ros2 service call /write_state cartographer_ros_msgs/srv/WriteState "{filename: '/home/rcv/SLAM/0320.pbstream'}"
-ros2 run nav2_map_server map_saver_cli -f /home/rcv/SLAM/0320
-
